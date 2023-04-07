6b108e0a4ce05089e9c4627fd13b781e0b3e3f56
Esther Onchieku <estheronchieku@gmail.com>
2023-04-07 12:20:17 -0500

    task6 completed

--- TechJobsOOAutoGraded6/Location.cs
+++ TechJobsOOAutoGraded6/Location.cs
@@ -1,40 +1,14 @@
 ﻿using System;
 namespace TechJobsOOAutoGraded6
 {
-    public class Location
+    public class Location:JobField
     {
-        public string Value { get; set; }
-        public int Id { get; }
-        private static int nextId = 1;
-
-        public Location()
-        {
-            Id = nextId;
-            nextId++;
+            public Location(string value) : base(value)
+            {
+            }
         }
-        public Location(string value) : this()
-        {
-            Value = value;
-        }
-       
 
         // TODO: Task 2: Add a second constructor to this class that uses the Location() constructor and sets the value of the value field.
+ }
 
-        public override bool Equals(object obj)
-        {
-            return obj is Location location &&
-                   Id == location.Id;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id);
-        }
-
-        public override string ToString()
-        {
-            return Value;
-        }
-    }
-}
 
